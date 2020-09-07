72e40e80c0809b5a2c3af82fcc0102c22b6d8577
Prabjyot19 <58730829+Prabjyot19@users.noreply.github.com>
2020-09-07 22:39:13 +0530

    Create shells.sh

---
+++ shells.sh
@@ -0,0 +1,3 @@
+#!/bin/bash                                                                                                                                 
+                                                                                                                                            
+echo -e "HURRAY!!!\nConnected BitBucket to Jenkins on HDP Sandbox"                                                                          
